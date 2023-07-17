9e13d24a1b1d891bc06322c94f3e795b27ff8361
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2023-07-17 16:45:53 +0100

    Added function to generate completed at datetimes and append them to action logs (and print to the terminal)

--- steps/common/pipeline.py
+++ steps/common/pipeline.py
@@ -1,6 +1,9 @@
 from typing import Dict, Optional
 import toml
 
+import datetime
+from rich import print
+
 def load_config() -> Dict:
     """ 
     Loads the configuration file for the pipline and returns a dictionary of values
@@ -43,3 +46,15 @@ def write_lockfile(filename:str, text:str):
     with open(filename, 'w') as lockfile:
         lockfile.write(text)
     pass
+
+
+def generate_completed_at_entry(action_log:Dict) -> Dict:
+    # generate the completed at variable
+    completed_at = datetime.datetime.now().isoformat()
+
+    action_log['completed_at'] = completed_at
+
+    # output completed at message to terminal
+    print (f"Completed at {completed_at}")
+
+    return action_log
