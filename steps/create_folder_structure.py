031e001e2dcf746e574bddedc372ed04b79bc90d
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2023-07-17 10:19:22 +0100

    Added step for create folder structure step

--- steps/create_folder_structure.py
+++ steps/create_folder_structure.py
@@ -1,7 +1,7 @@
 from typing import Dict
 import os
 
-def create_output_folder_structure(config:Dict):
+def create_output_folder_structure(config:Dict, verbose:bool=False):
     print ('make the directories')
     return {}
 
