fe8b9269e4ddc81f8c4b5d9b2b4e2a0f2f46ae53
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2023-07-18 11:07:33 +0100

    Refactored the pipeline in order to split some logic related to argument parsing into a main function

--- steps/run_pipeline.py
+++ steps/run_pipeline.py
@@ -1,3 +1,4 @@
+from typing import Dict
 
 from common.pipeline import Pipeline
 
@@ -6,65 +7,66 @@ from parse_class_i_bulk_data import construct_class_i_bulk_allele_lists
 from fetch_raw_data import fetch_raw_datasets
 from create_folder_structure import create_folder_structure
 
-
 from rich.console import Console
-console = Console()
 
-from rich import print
-
-import git
-
-steps = {
-    '1':{
-        'function':create_folder_structure,
-        'title_template':'Creating the folder structure in the output directory',
-        'list_item':'Creating the folder structure in the output directory'
-    },
-    '2':{
-        'function':fetch_raw_datasets,
-        'title_template':'Downloading latest versions of the IPD and H2 sequence datasets',
-        'list_item':'Downloading latest versions of the IPD and H2 sequence datasets'
-    },
-    '3':{
-        'function':construct_class_i_locus_allele_lists,
-        'title_template':'Parsing IPD sequence set for HLA-{locus}',
-        'list_item':'Parsing the human Class I sequences from IPD'
-    },
-    '4':{
-        'function':construct_class_i_bulk_allele_lists,
-        'title_template':'Parsing IPD sequence set for non-human Class I',
-        'list_item':'Parsing the non-human Class I sequences from IPD'
-    },
-    '5':{
-        'function':construct_class_i_locus_allele_lists,
-        'title_template':'Parsing H2 sequence set for H2-{locus}',
-        'list_item':'Parsing the mouse Class I sequences from a custom dataset'
+def run_pipeline(verbose:bool=False, force:bool=False, logoutput:bool=True) -> Dict:
+    steps = {
+        '1':{
+            'function':create_folder_structure,
+            'title_template':'Creating the folder structure in the output directory',
+            'list_item':'Creating the folder structure in the output directory'
+        },
+        '2':{
+            'function':fetch_raw_datasets,
+            'title_template':'Downloading latest versions of the IPD and H2 sequence datasets',
+            'list_item':'Downloading latest versions of the IPD and H2 sequence datasets'
+        },
+        '3':{
+            'function':construct_class_i_locus_allele_lists,
+            'title_template':'Parsing IPD sequence set for HLA-{locus}',
+            'list_item':'Parsing the human Class I sequences from IPD'
+        },
+        '4':{
+            'function':construct_class_i_bulk_allele_lists,
+            'title_template':'Parsing IPD sequence set for non-human Class I',
+            'list_item':'Parsing the non-human Class I sequences from IPD'
+        },
+        '5':{
+            'function':construct_class_i_locus_allele_lists,
+            'title_template':'Parsing H2 sequence set for H2-{locus}',
+            'list_item':'Parsing the mouse Class I sequences from a custom dataset'
+        }
     }
-}
 
-pipeline = Pipeline(steps, console, logoutput=True)
+    pipeline = Pipeline(steps, Console(), logoutput)
 
-pipeline.run_step('1')
+    pipeline.run_step('1')
 
-pipeline.run_step('2')
+    pipeline.run_step('2')
 
-hla_class_i = pipeline.get_config_item('HLA_CLASS_I')
+    hla_class_i = pipeline.get_config_item('HLA_CLASS_I')
 
-i = 1
-for locus in hla_class_i:
-    pipeline.run_step('3', substep=i, locus=locus, species_slug='hla', sequence_set='IPD_IMGT_HLA_PROT')
-    i+=1
+    i = 1
+    for locus in hla_class_i:
+        pipeline.run_step('3', substep=i, locus=locus, species_slug='hla', sequence_set='IPD_IMGT_HLA_PROT')
+        i+=1
 
-console.rule(title=f"4. ")
+    construct_class_i_bulk_allele_lists('IPD-MHC')
 
-construct_class_i_bulk_allele_lists('IPD-MHC')
+    h2_class_i = pipeline.get_config_item('H2_CLASS_I')
 
-h2_class_i = pipeline.get_config_item('H2_CLASS_I')
+    j = 1
+    for locus in h2_class_i:
+        pipeline.run_step('5', substep=j, locus=locus, species_slug='h2', sequence_set='H2_CLASS_I_PROT')
+        j+=1
+    
+    return {}
 
-j = 1
-for locus in h2_class_i:
-    pipeline.run_step('5', substep=j, locus=locus, species_slug='h2', sequence_set='H2_CLASS_I_PROT')
-    j+=1
+
+def main():
+    output = run_pipeline()
+
+main()
 
 
 
